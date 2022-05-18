09c6b92716e2375b2a9a73212967b898eb5d154c
anon <anon@anon.com>
2022-05-18 15:04:22 -0400

    working on oauth process

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,66 @@
 use std::collections::HashMap;
 
+// Actix and serde are only requried to get the auth code from twitter
+use actix_web::{get, web, App, HttpServer, HttpRequest, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct TwitterAuthResponse{
+	state:String,
+	code:String,
+}
 
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> std::io::Result<()> {
 	
+	// Generate auth link
 	let config = AuthConfig::new();
 	println!("Navigate to this link to authorize: {}",config.get_authorize_link());
+	
+	// Start listening, Twitter will send the auth code to us.
+	// We need to convert it to an access code within 30 seconds or it will expire.
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(|auth: web::Query<TwitterAuthResponse>| async move {
+				let access_code = transform_auth_to_access(&auth.code).await.unwrap();
+				""
+			}))
+    })
+    .bind(("127.0.0.1", 8080))?.run().await
+}
 
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
+// The auth code needs converted into an access code.
+async fn transform_auth_to_access(auth_code:&str) -> Result<String,Box<dyn std::error::Error>>{
+	let access_code = reqwest::Client::new()
+		//.post("https://api.twitter.com/2/oauth2/token")
+		.post("http://localhost.com:3000")
+		.form(&[
+			("code", auth_code),
+			("grant_type", "authorization_code"),
+			("client_id", env!("client_id")),
+			("redirect_uri", "http://localhost.com:8080"),
+			("code_verifier", "challenge"),
+		])
+		//.body("Testbody")
+		.send()
+		.await?;
+	dbg!(&access_code);
+	Ok("access_code".to_owned())
+}
+
+
+async fn access(access_code:&str) -> Result<(), Box<dyn std::error::Error>> {
+
+	// Use the acces code to get some information 
+	let client = reqwest::Client::new();
+	let resp = client
+		.get("https://api.twitter.com/2/users/me")
+		.header("AUTHORIZATION",format!("Bearer {}",access_code))
+		.send()
         .await?;
+        //.json::<HashMap<String, String>>()
+        //.await?;
     println!("{:#?}", resp);
     Ok(())
 }
@@ -28,9 +77,11 @@ impl AuthConfig {
 		Self {
 			endpoint:"https://twitter.com/i/oauth2/authorize",
 			redirect_uri:"http://localhost.com:8080",
-			scope:vec!["tweet.write","users.read"],
+			scope:vec!["tweet.read","tweet.write","users.read","offline.access"],
 		}
 	}
+
+	// This is the link that the user will have to click on to start the oauth process
 	fn get_authorize_link(&self) -> String {
 		format!(
 			"{endpoint}?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}&state=state&code_challenge={challenge}&code_challenge_method=plain",
@@ -40,8 +91,7 @@ impl AuthConfig {
 			redirect_uri = self.redirect_uri,
 			challenge    = "makethisrandomdata",
 		)
-
 	}
+
 }
 
-//https://twitter.com/i/oauth2/authorize?response_type=code&client_id=dGotejkwLTMtT0s4LXEzNFl6REU6MTpjaQ&redirect_uri=http://localhost&scope=tweet.write%20users.read&state=state&code_challenge=makethisrandomdata&code_challenge_method=plain
--- test.sh
+++ test.sh
@@ -2,8 +2,24 @@
 source ./env.sh
 
 
-# Test to just get a couple of tweets
-curl "https://api.twitter.com/2/tweets?ids=1261326399320715264,1278347468690915330" \
-  -H "Authorization: Bearer $bearer_token"
+
+# Create a server on localhost:8080 and listen
+#key=$(nc -l 8080 | grep -oP '(?<=code=).*?(?=\s)')
+#echo $key
+
+#curl --location --request POST 'https://api.twitter.com/2/oauth2/token' \
+#--header 'Content-Type: application/x-www-form-urlencoded' \
+#--data-urlencode 'code='$key \
+#--data-urlencode 'grant_type=authorization_code' \
+#--data-urlencode 'client_id='$client_id \
+#--data-urlencode 'redirect_uri=https://www.localhost.com:8080' \
+#--data-urlencode 'code_verifier=challenge'
 
 
+curl --location --request POST 'http://localhost.com:3000' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'code=testkey' \
+--data-urlencode 'grant_type=authorization_code' \
+--data-urlencode 'client_id='$client_id \
+--data-urlencode 'redirect_uri=https://www.localhost.com:8080' \
+--data-urlencode 'code_verifier=challenge'
