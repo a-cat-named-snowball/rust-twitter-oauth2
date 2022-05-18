9793cab59542ed6b8d737922ba88cfdbefaf6871
anon <anon@anon.com>
2022-05-17 20:50:24 -0400

    got a working authroize url

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	
+	let config = AuthConfig::new();
+	println!("Navigate to this link to authorize: {}",config.get_authorize_link());
+
     let resp = reqwest::get("https://httpbin.org/ip")
         .await?
         .json::<HashMap<String, String>>()
@@ -17,24 +21,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 struct AuthConfig {
 	endpoint:&'static str,
 	redirect_uri:&'static str,
-	scopes:Vec<&'static str>,
+	scope:Vec<&'static str>,
 }
 impl AuthConfig {
 	fn new() -> Self {
 		Self {
 			endpoint:"https://twitter.com/i/oauth2/authorize",
-			redirect_uri:"http://localhost",
-			scopes:vec!["tweet.write","users.read"],
+			redirect_uri:"http://localhost.com:8080",
+			scope:vec!["tweet.write","users.read"],
 		}
 	}
 	fn get_authorize_link(&self) -> String {
 		format!(
-			"{endpoint}?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&state=state&code_challenge={challenge}&code_challenge_method=plain",
+			"{endpoint}?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}&state=state&code_challenge={challenge}&code_challenge_method=plain",
 			endpoint     = self.endpoint,
 			client_id    = env!("client_id"),
+			scope        = self.scope.join("%20"),
 			redirect_uri = self.redirect_uri,
 			challenge    = "makethisrandomdata",
 		)
 
 	}
 }
+
+//https://twitter.com/i/oauth2/authorize?response_type=code&client_id=dGotejkwLTMtT0s4LXEzNFl6REU6MTpjaQ&redirect_uri=http://localhost&scope=tweet.write%20users.read&state=state&code_challenge=makethisrandomdata&code_challenge_method=plain
